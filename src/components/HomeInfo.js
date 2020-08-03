c6929f152790d9f66edddbeeb12038116aebe81e
Andres <andrescardoso.dev@hotmail.com>
2020-08-03 17:25:09 -0300

    dinamismo + consumo de json

--- src/components/HomeInfo.js
+++ src/components/HomeInfo.js
@@ -1,38 +1,26 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import Noticia from './Noticia';
+import Noticia, {useNews} from './Noticia';
 
 import f1 from '../assets/img/f1.png';
 import '../scss/components/HomeInfo.scss';
 
 export default function HomeInfo() {
+
+    const newsData = useNews();
+
     return (
         <div className="contenedor">
             <h2 className="titulo"><span>Últimas novedades</span><img src={f1} className="logo" alt="f1"/></h2>
-            <Container fluid >
-                <Row style={{'text-align': 'center'}}>
-                    <Col style={{'background': 'blue'}}>
-                        <Noticia title='noticia1' text='texto noticia 1' />
-                    </Col>
-                    <Col style={{'background':'red'}}>
-                        <Noticia title='noticia1' text='texto noticia 1' />
-                    </Col>
-                    <Col style={{'background':'green'}}>
-                        <Noticia title='noticia1' text='texto noticia 1' />
-                    </Col>
-                </Row>
-                <Row style={{'text-align': 'center'}}>
-                    <Col style={{'background': 'yellow'}}>
-                        <Noticia title='noticia1' text='texto noticia 1' />
-                    </Col>
-                    <Col style={{'background':'grey'}}>
-                        <Noticia title='noticia1' text='texto noticia 1' />
-                    </Col>
-                    <Col style={{'background':'brown'}}>
-                        <Noticia title='noticia1' text='texto noticia 1' />
-                    </Col>
-                </Row>
-            </Container>
+            <Row md={3} className="news-row">
+            {
+                newsData.map(item => (  
+                        <Noticia title={item.title} img={item.img} />
+                ))
+            }
+            </Row>
+            
+            
         </div>
     );
 }
--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../scss/imghover.css';
 
-export default function Noticia() {
+export default function Noticia(props) {
     return (
-              <div className="hovereffect">
-            <img className="img-responsive" src="http://placehold.it/350x250" alt="imagen-noticia" />
+        <div className="hovereffect">
+            <img className="img-responsive" src={props.img} />
             <div className="overlay">
-                <h2>Effect 12</h2>
+                <h2>{props.title}</h2>
 				<p>
 					<a href="/">LINK HERE</a>
 				</p>
             </div>
         </div>  
-            );
+    );
+}
+
+export function useNews() { 
+    const newsJSON = require ('../assets/json/news.json');
+    const loadData = () => JSON.parse(JSON.stringify(newsJSON));
+
+    const [news, setNews] = useState([])
+    
+    useEffect(() => {
+        setNews(loadData)
+        console.log(news)
+    }, [])
+
+    /*useEffect(() => {
+        fetch(newsJSON)
+        .then(response => response.json())
+        .then(datos => {    
+          setNews(datos)
+        })
+    }, [])*/
+   
+    return news
 }
